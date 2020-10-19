7d0c5530ab756fd3ddb1b702922b02ca3d265b29
Sergio García <44734540+Booker141@users.noreply.github.com>
2020-10-19 12:34:49 +0200

    Comenzando index.js

---
+++ sweet-home/pages/Login/index.js
@@ -0,0 +1,9 @@
+import styles from '../styles/globals.css' 
+
+export default function Contact(){
+
+    return()
+
+   
+
+}
--- sweet-home/pages/_app.js
+++ sweet-home/pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../styles/HomeStyle.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
