49a60f85d6beda7c84f7a23ef1e2626dfedf7be7
Sergio <sergiogn2010@hotmail.es>
2022-07-07 12:50:33 +0200

    Update loading animation between pages

--- sweet-home/pages/_app.js
+++ sweet-home/pages/_app.js
@@ -1,12 +1,39 @@
 import Layout from 'components/Layout/Layout';
 import 'styles/styles.module.css'
 import {SessionProvider} from 'next-auth/react'
+import {useRouter} from 'next/router'
+import {useEffect} from 'react'
+import {useState} from 'react'
 
+/* Animación de carga entre páginas*/
+function Loading(){
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const handleStart = (url) => (url !== router.asPath) && setLoading(true);
+    const handleEnd = (url) => (url === router.asPath) && setLoading(false);
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleEnd);
+    router.events.on('routeChangeError', handleEnd);
+
+    return() => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleEnd);
+      router.events.off('routeChangeError', handleEnd);
+    }
+
+  })
+  return loading ? <div className="loading">
+    <div className="loading">
+    </div>
+  </div> : null;
+}
 function MyApp({ Component, pageProps: {session, ...pageProps} }) {
 
   return(
     
       <SessionProvider session={session}>
+        <Loading/>
         <Layout>
           <Component {...pageProps} />
         </Layout>
